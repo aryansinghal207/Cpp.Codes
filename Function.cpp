33502ff4384a596c54cc6fa3d07661964eed0adb
Aryan <aryansinghal207@gmail.com>
2024-07-03 11:29:08 +0530

    g

--- Function.cpp
+++ Function.cpp
@@ -4,9 +4,16 @@ int  add(int num1,int num2){
     int sum=num1+num2;
     return sum;
 }
+int add(int num1,int num2,int num3){
+    int sum=num1+num2+num3;
+    return sum;
+}
 int main(){
-    int a;int b;
+    int a;int b;int c;
     cin>>a;
     cin>>b;
+    cin>>c;
     cout<<add(a,b);
+    cout<<endl;
+    cout<<add(a,b,c);
 }
