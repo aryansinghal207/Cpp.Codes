6283ceb3bb107514bcfef09851b974dffe082881
Aryan <aryansinghal207@gmail.com>
2024-07-03 11:38:46 +0530

    g

--- Function.cpp
+++ Function.cpp
@@ -1,19 +1,26 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int  add(int num1,int num2){
+//     int sum=num1+num2;
+//     return sum;
+// }
+// int add(int num1,int num2,int num3){
+//     int sum=num1+num2+num3;
+//     return sum;
+// }
+// int main(){
+//     int a;int b;int c;
+//     cin>>a;
+//     cin>>b;
+//     cin>>c;
+//     cout<<add(a,b);
+//     cout<<endl;
+//     cout<<add(a,b,c);
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
-int  add(int num1,int num2){
-    int sum=num1+num2;
-    return sum;
-}
-int add(int num1,int num2,int num3){
-    int sum=num1+num2+num3;
-    return sum;
-}
 int main(){
-    int a;int b;int c;
-    cin>>a;
-    cin>>b;
-    cin>>c;
-    cout<<add(a,b);
-    cout<<endl;
-    cout<<add(a,b,c);
+    cout<<pow(2,4)<<endl;
+    cout<<sqrt(16)<<endl;
 }
