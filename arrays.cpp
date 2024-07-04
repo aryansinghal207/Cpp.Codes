cdef38857d47fcd32da0de9ebf57b84d8912ef7d
Aryan <aryansinghal207@gmail.com>
2024-07-04 12:19:23 +0530

    g

--- arrays.cpp
+++ arrays.cpp
@@ -1,12 +1,30 @@
+// ### To find maximum element of an array.
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int arr[]={1,2,7,5,18};
+//     int max=arr[0];
+//     for(int i=1;i<5;i++){
+//         if(arr[i]>max){
+//             max=arr[i];
+//         }
+//     }
+//     cout<<max<<endl;
+// }
+
+
+// ### Linear Search
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
     int arr[]={1,2,7,5,18};
-    int max=arr[0];
-    for(int i=1;i<5;i++){
-        if(arr[i]>max){
-            max=arr[i];
+    int num;
+    cin>>num;
+    int ind=-1;
+    for(int i=0;i<5;i++){
+        if(arr[i]==num){
+            ind=i;
         }
     }
-    cout<<max<<endl;
+    cout<<ind<<endl;
 }
