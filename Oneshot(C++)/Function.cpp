a6201308091b940c21850d2841b3a5699c77d1ca
Aryan <aryansinghal207@gmail.com>
2024-07-30 11:15:23 +0530

    g

--- Oneshot(C++)/Function.cpp
+++ Oneshot(C++)/Function.cpp
@@ -1,23 +1,45 @@
+// Combination
+// #include<bits/stdc++.h>
+// using namespace std;
+// int fact(int x){
+//     int f=1;
+//     for(int i=1;i<=x;i++){
+//         f *= i;
+//     }
+//     return f;
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter n : ";
+//     cin>>n;
+//     int r;
+//     cout<<"Enter r : ";
+//     cin>>r;
+//     int a=fact(n);
+//     int b=fact(r);
+//     int c=fact(n-r);
+//     cout<<a/(b*c);
+// }
+
+
+// Permutation
 #include<bits/stdc++.h>
 using namespace std;
 int fact(int x){
     int f=1;
     for(int i=1;i<=x;i++){
-        f *= i;
+        f*=i;
     }
     return f;
 }
 int main(){
     int n;
-    cout<<"Enter n : ";
+    cout<<"Enter the value of n : ";
     cin>>n;
     int r;
-    cout<<"Enter r : ";
+    cout<<"Enter the value of r : ";
     cin>>r;
     int a=fact(n);
     int b=fact(r);
-    int c=fact(n-r);
-    cout<<a/(b*c);
+    cout<<a/b;
 }
-
-
