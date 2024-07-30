7f778c0b3886d933c3f18d6cec535f54ac97c18d
Aryan <aryansinghal207@gmail.com>
2024-07-30 23:07:31 +0530

    g

--- Oneshot(C++)/Function.cpp
+++ Oneshot(C++)/Function.cpp
@@ -23,23 +23,49 @@
 
 
 // Permutation
+// #include<bits/stdc++.h>
+// using namespace std;
+// int fact(int x){
+//     int f=1;
+//     for(int i=1;i<=x;i++){
+//         f*=i;
+//     }
+//     return f;
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter the value of n : ";
+//     cin>>n;
+//     int r;
+//     cout<<"Enter the value of r : ";
+//     cin>>r;
+//     int a=fact(n);
+//     int b=fact(r);
+//     cout<<a/b;
+// }
+
+
+// Pascal Triangle
 #include<bits/stdc++.h>
 using namespace std;
 int fact(int x){
     int f=1;
     for(int i=1;i<=x;i++){
-        f*=i;
+        f *= i;
     }
     return f;
 }
+int ncr(int n,int r){
+    return fact(n)/(fact(r)*fact(n-r));
+}
 int main(){
-    int n;
-    cout<<"Enter the value of n : ";
-    cin>>n;
-    int r;
-    cout<<"Enter the value of r : ";
-    cin>>r;
-    int a=fact(n);
-    int b=fact(r);
-    cout<<a/b;
+    int a;
+    cout<<"Enter the number : ";
+    cin>>a;
+    for(int i=0;i<=a;i++){
+        for(int j=0;j<=i;j++){
+            cout<<ncr(i,j)<<" ";
+        }
+        cout<<endl;
+    }
 }
