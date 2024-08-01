03661072aa0c9385be42b33f5cc0e3a46fced010
Aryan <aryansinghal207@gmail.com>
2024-08-01 11:19:35 +0530

    g

--- Oneshot(C++)/array.cpp
+++ Oneshot(C++)/array.cpp
@@ -18,19 +18,40 @@
 
 
 // Find the product of the element of array using function
+// #include<iostream>
+// using namespace std;
+// void pro(int arr[5]){
+//     int mul=1;
+//     for(int i=0;i<5;i++){
+//         mul=mul*arr[i];
+//     }
+//     cout<<mul;
+// }
+// int main(){
+//     int arr[5];
+//     for(int i=0;i<5;i++){
+//         cin>>arr[i];
+//     }
+//     pro(arr);
+// }
+
+
+// Find the maximum element from the array
 #include<iostream>
 using namespace std;
-void pro(int arr[5]){
-    int mul=1;
+void findmax(int arr[5]){
+    int max=arr[0];
     for(int i=0;i<5;i++){
-        mul=mul*arr[i];
+        if(arr[i]>max){
+            max=arr[i];
+        }
     }
-    cout<<mul;
+    cout<<max;
 }
 int main(){
     int arr[5];
     for(int i=0;i<5;i++){
         cin>>arr[i];
     }
-    pro(arr);
+    findmax(arr);
 }
