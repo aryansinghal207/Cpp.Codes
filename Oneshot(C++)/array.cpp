a1863d54e2309ece46c53cab7bb19807b874d3cd
Aryan <aryansinghal207@gmail.com>
2024-08-01 11:10:06 +0530

    g

--- Oneshot(C++)/array.cpp
+++ Oneshot(C++)/array.cpp
@@ -1,17 +1,36 @@
 // Add the array using function
+// #include<iostream>
+// using namespace std;
+// void add(int arr[5]){
+//     int sum=0;
+//     for(int i=0;i<5;i++){
+//         sum=sum+arr[i];
+//     }
+//     cout<<sum;
+// }
+// int main(){
+//     int arr[5];
+//     for(int i=0;i<5;i++){
+//         cin>>arr[i];
+//     }
+//     add(arr);
+// }
+
+
+// Find the product of the element of array using function
 #include<iostream>
 using namespace std;
-void add(int arr[5]){
-    int sum=0;
+void pro(int arr[5]){
+    int mul=1;
     for(int i=0;i<5;i++){
-        sum=sum+arr[i];
+        mul=mul*arr[i];
     }
-    cout<<sum;
+    cout<<mul;
 }
 int main(){
     int arr[5];
     for(int i=0;i<5;i++){
         cin>>arr[i];
     }
-    add(arr);
+    pro(arr);
 }
