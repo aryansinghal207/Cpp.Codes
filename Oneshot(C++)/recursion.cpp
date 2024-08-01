349554acb061449690c4ec4672ffb2adbe86d3a4
Aryan <aryansinghal207@gmail.com>
2024-08-01 09:04:06 +0530

    g

--- Oneshot(C++)/recursion.cpp
+++ Oneshot(C++)/recursion.cpp
@@ -1,15 +1,31 @@
+// ## Print the number from 1 to n (Parameterised)
+// #include<iostream>
+// using namespace std;
+// void print(int n,int x){
+//     if(n==x+1) return;
+//     cout<<n<<endl;
+//     print(n+1,x);
+// }
+// int main(){
+//     int a,b;
+//     cout<<"Enter the value of a :";
+//     cin>>a;
+//     cout<<"Enter the last value : ";
+//     cin>>b;
+//     print(a,b);
+// }
+
+
+// ## Return the sum from 1 to n 
 #include<iostream>
 using namespace std;
-void print(int n,int x){
-    if(n==x+1) return;
-    cout<<n<<endl;
-    print(n+1,x);
+int sum(int n){
+    if(n==1) return 1;
+    return n + sum(n-1);
 }
 int main(){
-    int a,b;
+    int a;
     cout<<"Enter the value of a :";
     cin>>a;
-    cout<<"Enter the last value : ";
-    cin>>b;
-    print(a,b);
+    cout<<sum(a);
 }
