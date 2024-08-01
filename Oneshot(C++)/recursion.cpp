57471800de36ed7d774a2f870c61ffb3a7379f29
Aryan <aryansinghal207@gmail.com>
2024-08-01 10:55:56 +0530

    g

--- Oneshot(C++)/recursion.cpp
+++ Oneshot(C++)/recursion.cpp
@@ -47,17 +47,32 @@
 
 
 // ## Print the value of a raised to the power b
+// #include<iostream>
+// using namespace std;
+// int power(int n,int x){
+//     if(x==0) return 1;
+//     return n*power(n,x-1);
+// }
+// int main(){
+//     int a,b;
+//     cout<<"Enter the value of a :";
+//     cin>>a;
+//     cout<<"Enter the last value : ";
+//     cin>>b;
+//     cout<<power(a,b);
+// }
+
+
+// Print fibonacci series usng recursion
 #include<iostream>
 using namespace std;
-int power(int n,int x){
-    if(x==0) return 1;
-    return n*power(n,x-1);
+int fibo(int n){
+    if(n==0 || n==1) return 1;
+    return fibo(n-1)*fibo(n-2);
 }
 int main(){
     int a,b;
     cout<<"Enter the value of a :";
     cin>>a;
-    cout<<"Enter the last value : ";
-    cin>>b;
-    cout<<power(a,b);
+    cout<<"The fibo of"<<a<<"is : "<<fibo(a);
 }
