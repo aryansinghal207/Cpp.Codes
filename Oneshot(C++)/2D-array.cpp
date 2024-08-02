a1177059a99ddd1de35aacf380e83c9b3cf36775
Aryan <aryansinghal207@gmail.com>
2024-08-02 17:37:08 +0530

    g

--- Oneshot(C++)/2D-array.cpp
+++ Oneshot(C++)/2D-array.cpp
@@ -2,13 +2,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[2][3];
-    arr[0][0]=1;
-    arr[0][1]=2;
-    arr[0][2]=3;
-    arr[1][0]=4;
-    arr[1][1]=5;
-    arr[1][2]=6;
+    int arr[2][3]={{1,2,3},{4,5,6}};
     cout<<"The Actual Matrix is : ";
     for(int i=0;i<2;i++){
         for(int j=0;j<3;j++){
@@ -24,3 +18,5 @@ int main(){
         cout<<endl;
     }
 }
+
+
