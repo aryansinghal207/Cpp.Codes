a60de7285a1a3ea22184830f1cafad90ee4c6f87
Aryan <aryansinghal207@gmail.com>
2024-08-04 09:25:24 +0530

    g

--- Oneshot(C++)/2D-array.cpp
+++ Oneshot(C++)/2D-array.cpp
@@ -21,40 +21,43 @@
 
 
 // WAP to add 2 matrixes
-#include<iostream>
-using namespace std;
-int main(){
-    int row;
-    int column;
-    cout<<"Enter number of rows : ";
-    cin>>row;
-    cout<<"Enter number of colums : ";
-    cin>>column;
-    int mat1[row][column],mat2[row][column],summat[row][column];
-    cout<<"Enter element of first matrices : "<<endl;
-    for(int i=0;i<row;i++){
-        for(int j=0;j<column;j++){
-            cout<<"Element [" << i << "," << j << "]: ";
-            cin>>mat1[i][j];
-        }
-    }
-    cout<<"Enter element of Second matrices : "<<endl;
-    for(int i=0;i<row;i++){
-        for(int j=0;j<column;j++){
-            cout<<"Element [" << i << "," << j << "]: ";
-            cin>>mat2[i][j];
-        }
-    }
-    for(int i=0;i<row;i++){
-        for(int j=0;j<column;j++){
-            summat[i][j]=mat1[i][j]+mat2[i][j];
-        }
-    }
-    cout<<"The elements of resultant matrices : "<<endl;
-    for(int i=0;i<row;i++){
-        for(int j=0;j<column;j++){
-            cout<<summat[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row;
+//     int column;
+//     cout<<"Enter number of rows : ";
+//     cin>>row;
+//     cout<<"Enter number of colums : ";
+//     cin>>column;
+//     int mat1[row][column],mat2[row][column],summat[row][column];
+//     cout<<"Enter element of first matrices : "<<endl;
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<column;j++){
+//             cout<<"Element [" << i << "," << j << "]: ";
+//             cin>>mat1[i][j];
+//         }
+//     }
+//     cout<<"Enter element of Second matrices : "<<endl;
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<column;j++){
+//             cout<<"Element [" << i << "," << j << "]: ";
+//             cin>>mat2[i][j];
+//         }
+//     }
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<column;j++){
+//             summat[i][j]=mat1[i][j]+mat2[i][j];
+//         }
+//     }
+//     cout<<"The elements of resultant matrices : "<<endl;
+//     for(int i=0;i<row;i++){
+//         for(int j=0;j<column;j++){
+//             cout<<summat[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// 
---
+++ Oneshot(C++)/string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // string x="Aryan is learning c++";
+    // cout<<x;
+    string s;
+    cout<<"Enter a string  : ";
+    getline(cin,s);
+    cout<<s;
+}
