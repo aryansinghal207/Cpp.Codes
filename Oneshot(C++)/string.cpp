cadbed163a35e286a860ca8fd850ae43997fb128
Aryan <aryansinghal207@gmail.com>
2024-08-04 09:27:32 +0530

    g

--- Oneshot(C++)/string.cpp
+++ Oneshot(C++)/string.cpp
@@ -1,10 +1,20 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     // string x="Aryan is learning c++";
+//     // cout<<x;
+//     string s;
+//     cout<<"Enter a string  : ";
+//     getline(cin,s);
+//     cout<<s;
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    // string x="Aryan is learning c++";
-    // cout<<x;
-    string s;
-    cout<<"Enter a string  : ";
-    getline(cin,s);
-    cout<<s;
+    string x="Aryan is learning c++";
+    cout<<x<<endl;
+    x[0]='a';
+    cout<<x;
 }
