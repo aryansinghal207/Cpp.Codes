eb78c3976f599fb6e02a11231998f32853a8b0c7
Aryan <aryansinghal207@gmail.com>
2024-08-04 09:45:13 +0530

    g

--- Oneshot(C++)/string.cpp
+++ Oneshot(C++)/string.cpp
@@ -10,11 +10,15 @@
 // }
 
 
-#include<iostream>
-using namespace std;
-int main(){
-    string x="Aryan is learning c++";
-    cout<<x<<endl;
-    x[0]='a';
-    cout<<x;
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     string x="Aryan is learning c++";
+//     cout<<x<<endl;
+//     cout<<x.length();
+// }
+
+
+// Push back :- By using this we can add a character at the end of the existing string.
+// POP back :- By using this we can remove a character at the end of the existing string.
+// Append :- By using this we can add a string at the end of the existing string.
