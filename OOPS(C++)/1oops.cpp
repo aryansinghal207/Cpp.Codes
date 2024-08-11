a04da06c04cb1e0cf9186b56fb88607b7508a1f9
Aryan <aryansinghal207@gmail.com>
2024-08-11 23:15:20 +0530

    g

--- OOPS(C++)/1oops.cpp
+++ OOPS(C++)/1oops.cpp
@@ -1,17 +1,66 @@
+// #A class is a user defined datatype #
+// #include<iostream>
+// using namespace std;
+// class student{
+//     // Default this is private and can't be accessable outside the function.
+//     public:
+//     string name;
+//     int age, rollno;
+//     string grade;
+// };
+// int main(){
+//     student s1;
+//     s1.name="Aryan";
+//     s1.age=18;
+//     s1.rollno=133;
+//     s1.grade="O+";
+//     cout<<s1.age<<endl;
+// }
+
+
 #include<iostream>
 using namespace std;
 class student{
-    // Default this is private and can't be accessable outside the function.
-    public:
+    private:
     string name;
     int age, rollno;
     string grade;
+    // Function Getter and Setter
+    public:
+    void setname(string a){
+        name=a;
+    }
+    void setage(int b){
+        age=b;
+    }
+    void setrollno(int c){
+        rollno=c;
+    }
+    void setgrade(string d){
+        grade=d;
+    }
+
+    void getname(){
+        cout<<name<<endl;
+    }
+    void getage(){
+        cout<<age<<endl;
+    }
+    void getrollno(){
+        cout<<rollno<<endl;
+    }
+    void getgrade(){
+        cout<<grade<<endl;
+    }
 };
 int main(){
     student s1;
-    s1.name="Aryan";
-    s1.age=18;
-    s1.rollno=133;
-    s1.grade="O+";
-    cout<<s1.age<<endl;
+    s1.setname("Aryan");
+    s1.setage(18);
+    s1.setrollno(133);
+    s1.setgrade("O+");
+    s1.getname();
+    s1.getage();
+    s1.getrollno();
+    s1.getgrade();
 }
