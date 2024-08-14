94fdfb3061912662b82ee44fd4ed80220cfdc9f0
Aryan <aryansinghal207@gmail.com>
2024-08-14 23:14:39 +0530

    g

--- OOPS(C++)/Constructor.cpp
+++ OOPS(C++)/Constructor.cpp
@@ -1,22 +1,3 @@
-// #include<iostream>
-// using namespace std;
-// class customer{
-//     string name;
-//     int account;
-//     int balance;
-//     customer(){
-//         cout<<"Constructor is called : ";
-//     }
-//     customer(string a,int b,int c){
-//         name=a;
-//         account=b;
-//         balance=c;
-//     }
-// };
-// int main(){
-//     customer A1("Rohit",123,10000);
-// }
-
 #include<iostream>
 using namespace std;
 class customer {
