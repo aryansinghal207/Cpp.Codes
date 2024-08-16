db11426c3336435ccd0027e2e2871aaf8cf17083
Aryan <aryansinghal207@gmail.com>
2024-08-16 09:49:59 +0530

    g

--- OOPS(C++)/Constructor.cpp
+++ OOPS(C++)/Constructor.cpp
@@ -1,30 +1,51 @@
+#include<iostream>
+using namespace std;
+class customer {
+    string name;
+    int account;
+    int balance;
+public:
+//Default Constructor
+    customer() {
+        cout << "Default constructor is called." << endl;
+        name="Tohit";
+        account=987;
+        balance=345;
+    }
+//Parametrized Constructor 
+    customer(string a, int b, int c) {
+        name = a;
+        account = b;
+        balance = c;
+        cout << "Parameterized constructor is called." << endl;
+    }
+    // Constructor overloading  :- when a constructor takes different number of inputs and shows the result. 
+    customer(string a, int c) {
+        // this-> :- It is used to store the pointer value.
+        this->name = a;
+        this->balance = c;
+        cout << "Parameterized constructor is called." << endl;
+    }
+    // Inline constructor :- This is showing error bcz compiler can't decide from whom it should take the parameterized value.
+    // inline customer(string a,int b,int c): name(a),account(b),balance(c){
+    // }
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Account: " << account << endl;
+        cout << "Balance: " << balance << endl;
+    }
+};
+int main() {
+    customer A1;
+    customer A2("Rohit", 123, 10000);
+    customer A3("Mohit",10000);
+    A1.display();
+    A2.display();
+    A3.display();
+    return 0;
+}
+
+
+// // 
 // #include<iostream>
 // using namespace std;
-// class customer {
-//     string name;
-//     int account;
-//     int balance;
-// public:
-//     customer() {
-//         cout << "Default constructor is called." << endl;
-//     }
-//     customer(string a, int b, int c) {
-//         name = a;
-//         account = b;
-//         balance = c;
-//         cout << "Parameterized constructor is called." << endl;
-//     }
-//     void display() {
-//         cout << "Name: " << name << endl;
-//         cout << "Account: " << account << endl;
-//         cout << "Balance: " << balance << endl;
-//     }
-// };
-// int main() {
-//     customer A1("Rohit", 123, 10000);
-//     A1.display();
-//     return 0;
-// }
-
-
-
--- OOPS(C++)/tempCodeRunnerFile.cpp
+++ OOPS(C++)/tempCodeRunnerFile.cpp
@@ -1,18 +1,33 @@
 #include<iostream>
-// using namespace std;
-// class customer{
-//     string name;
-//     int account;
-//     int balance;
-//     customer(){
-//         cout<<"Constructor is called : ";
-//     }
-//     customer(string a,int b,int c){
-//         name=a;
-//         account=b;
-//         balance=c;
-//     }
-// };
-// int main(){
-//     customer A1("Rohit",123,10000);
-// }
+using namespace std;
+class customer {
+    string name;
+    int account;
+    int balance;
+public:
+//Default Constructor
+    customer() {
+        cout << "Default constructor is called." << endl;
+    }
+//Parametrized Constructor 
+    customer(string a, int b, int c) {
+        name = a;
+        account = b;
+        balance = c;
+        cout << "Parameterized constructor is called." << endl;
+    }
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Account: " << account << endl;
+        cout << "Balance: " << balance << endl;
+    }
+};
+int main() {
+    customer A1;
+    customer A2("Rohit", 123, 10000);
+    customer A3("Mohit",321,10000);
+    A1.display();
+    A2.display();
+    A3.display();
+    return 0;
+}
